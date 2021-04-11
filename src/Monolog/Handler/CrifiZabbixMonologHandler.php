fef64e753a682a597377bf06e16050ee040abe37
Christoph Fischer <christoph@wecanweb.de>
2021-04-11 15:21:30 +0200

    Include a monolog handler for logging to zabbix trapper items

---
+++ src/Monolog/Handler/CrifiZabbixMonologHandler.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Crifi\CrifiZabbixBundle\Monolog\Handler;
+
+use Disc\Zabbix\Sender;
+use Monolog\Handler\AbstractProcessingHandler;
+use Monolog\Logger;
+
+class ZabbixMonologHandler extends AbstractProcessingHandler
+{
+    private $zabbixHost;
+
+    private $zabbixPort;
+
+    private $zabbixTrapperHost;
+
+    private $zabbixTrapperKey;
+
+    public function __construct(
+        string $zabbixHost,
+        int $zabbixPort,
+        string $zabbixTrapperHost,
+        string $zabbixTrapperKey,
+        $level = Logger::DEBUG,
+        bool $bubble = true
+    ) {
+        $this->zabbixHost = $zabbixHost;
+        $this->zabbixPort = $zabbixPort;
+        $this->zabbixTrapperHost = $zabbixTrapperHost;
+        $this->zabbixTrapperKey = $zabbixTrapperKey;
+        parent::__construct($level, $bubble);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handleBatch(array $records): void
+    {
+        $messages = [];
+
+        foreach ($records as $record) {
+            if ($record['level'] < $this->level) {
+                continue;
+            }
+            $messages[] = $this->processRecord($record);
+        }
+
+        if (!empty($messages)) {
+            $this->send($messages);
+        }
+    }
+
+    /**
+     * Sends a zabbix trapper message to zabbix
+     *
+     * @param array $records the array of log records that formed this content
+     */
+    protected function send(array $records)
+    {
+        $sender = new Sender($this->zabbixHost, $this->zabbixPort);
+        $sender->addData($this->zabbixTrapperHost, $this->zabbixTrapperKey, $this->getHighestRecord($records));
+        $sender->send();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    protected function write(array $record): void
+    {
+        $this->send([$record]);
+    }
+
+    protected function getHighestRecord(array $records): array
+    {
+        $highestRecord = null;
+        foreach ($records as $record) {
+            if (null === $highestRecord || $highestRecord['level'] < $record['level']) {
+                $highestRecord = $record;
+            }
+        }
+
+        return $highestRecord;
+    }
+}
